86e22cc64e14c6129af59a2eaec5d5dfdc66cc5e
Andrew Goeden <andrewgoeden@gmail.com>
2021-04-22 02:43:09 -0400

    basic react app logs incoming messages from socket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, {Component} from "react";
+// import io from "socket.io";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ws = new WebSocket("ws://localhost:5000/point");
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataFromServer: []
+    }
+  }
+
+  componentDidMount() {
+    ws.onopen = () => {
+      console.log("connected");
+    };
+
+    ws.onmessage = (msg) => {
+      const message = JSON.parse(msg.data);
+      this.state.dataFromServer.push(message);
+      console.log(this.state.dataFromServer);
+    };
+
+    ws.onclose = () => {
+      console.log("disconnected");
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
